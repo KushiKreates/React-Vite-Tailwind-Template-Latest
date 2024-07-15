5e7896301f14c86645a1646c26fb3ac237b454f6
Daisy <66263341+bloomsirenix@users.noreply.github.com>
2024-07-15 16:32:39 +0200

    Revert "removed token and enhanced security"
    This reverts commit cfa702d6d059f0dbf743b92f8d375fd079afcba0.

--- src/Pages/proxmoxApi.js
+++ src/Pages/proxmoxApi.js
@@ -1,13 +1,11 @@
+// src/api/proxmoxApi.js
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const instance = axios.create({
-  baseURL: process.env.PROXMOX_API_BASE_URL,
+  baseURL: 'https://81.169.237.72:8006/api2/json',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: process.env.PROXMOX_API_TOKEN,
+    Authorization: 'PVEAPIToken=API@pve!dash-app=0e39b17b-1e8e-490c-9251-bc24407c0f5c',
   },
   // Axios does not support httpsAgent in browser environments
   // Configure https options directly in the request
